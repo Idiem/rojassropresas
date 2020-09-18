5fdbf1450e7dacfdbfe5670a82812730349a9995
iDiem <dylan.sr2002@gmail.com>
2020-09-17 20:44:15 -0500

    arreglando router v1.8

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Inicio from './components/inicio'
-import {Router,Route,Switch} from 'react-router-dom'
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import Producto from './components/producto_id'
 import _404 from './components/NotFound'
 import history from './history';
@@ -10,8 +10,8 @@ function App() {
   return (
     <Router history={history}>
       <Switch>
-        <Route exact path='/'><Inicio/></Route>
-        <Route exact path='/producto/:id'> <Producto/> </Route>
+        <Route exact path='/' component={Inicio} />
+        <Route exact path='/producto/:id' component={Producto}/>
         <Route path="/" component={_404} />
       </Switch>
     </Router>
